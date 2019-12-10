164c22a85d86476b305486bd4ca20b49232f95e3
Richard Huelves <rhuelves@gmail.com>
2019-12-10 13:19:55 -0500

    finished signup route and signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-    
+
+    get 'users/:slug' do 
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/index'
+    end 
+
+    get '/signup' do 
+        if !logged_in?
+            erb :'users/signup'
+        else 
+            redirect to '/sneakers'
+        end 
+    end 
+
 end #end of UsersController 
