d2b5afc6f6785fe4b14dde70da0e00da1553fa14
Richard Huelves <rhuelves@gmail.com>
2019-12-11 11:13:20 -0500

    finished index, new, and show sneakers controller and views

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,4 +1,50 @@
 class SneakersController < ApplicationController
 
+    get '/sneakers' do 
+        if logged_in?
+            sneakers = Sneaker.all
+            @user = User.find(session[:user_id])
+            @user_sneakers = sneakers.select do |sneaker|
+                sneaker.user_id == @user.user.id 
+            end
+            erb :'sneakers/index'
+        else
+            redirect to '/login'
+        end 
+    end 
+
+    get '/sneakers/new' do 
+        if logged_in?
+            erb :'sneakers/new'
+        else 
+            redirect to '/login'
+        end 
+    end 
+
+    post '/sneakers' do
+        if logged_in?
+            if params[:brand] == "" || params[:model] == "" || params[:colorway] == "" || params[:cost] == ""
+                redirect to 'sneakers/new'
+            else
+                @sneaker = current_user.sneakers.build(brand: params[:brand], model: params[:model], colorway: params[:colorway], cost: params[:cost])
+                if @sneaker.save
+                    redirect to "sneakers/#{@sneaker.id}"
+                else 
+                    redirect to 'sneakers/new'
+                end 
+            end 
+        else 
+            redirect to '/login'
+        end 
+    end 
+
+    get '/sneakers/:id' do 
+        if logged_in?
+            @sneaker = Sneaker.find(params[:id])
+            erb :'sneakers/show'
+        else 
+            redirect to '/login'
+        end 
+    end 
 
 end #end of SneakersController 
