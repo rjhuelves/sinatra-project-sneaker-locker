5a499472a37a29b8e56b4d50b40b3dd9f8e1eeb6
Richard Huelves <rhuelves@gmail.com>
2019-12-11 12:26:52 -0500

    fisnished patch and delete in sneaker controller

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -5,7 +5,7 @@ class SneakersController < ApplicationController
             sneakers = Sneaker.all
             @user = User.find(session[:user_id])
             @user_sneakers = sneakers.select do |sneaker|
-                sneaker.user_id == @user.user.id 
+                sneaker.user_id == @user.id 
             end
             erb :'sneakers/index'
         else
@@ -46,5 +46,52 @@ class SneakersController < ApplicationController
             redirect to '/login'
         end 
     end 
+
+    get '/sneakers/:id/edit' do 
+        if logged_in?
+            @sneaker = Sneaker.find(params[:id])
+            if @sneaker && @sneaker.user == current_user
+                erb :'sneakers/edit'
+            else
+                redirect to '/sneakers'
+            end 
+        else
+            redirect to '/login'
+        end 
+    end 
+
+    patch '/sneakers/:id' do 
+        if logged_in?
+            if params[:brand] == "" || params[:model] == "" || params[:colorway] == "" || params[:cost] == ""
+                redirect to "/sneakers/#{params[:id]}/edit"
+            else
+                @sneaker = Sneaker.find(params[:id])
+                if @sneaker && @sneaker.user == current_user
+                    if @sneaker.update(brand: params[:brand], model: params[:model], colorway: params[:colorway], cost: params[:cost])
+                        redirect to "/sneakers/#{@sneaker.id}"
+                    else 
+                        redirect to "/sneakers/#{@sneaker.id}/edit"
+                    end 
+                else 
+                    redirect to '/sneakers'
+                end 
+            end
+        else 
+            redirect to '/login'
+        end 
+    end  
+
+    delete '/sneakers/:id/delete' do 
+        if logged_in?
+            @sneaker = Sneaker.find(params[:id])
+            if @sneaker && @sneaker.user == current_user
+                @sneaker.delete
+            end 
+            redirect to '/sneakers'
+        else
+            redirect to '/login'
+        end 
+    end 
+
 
 end #end of SneakersController 
